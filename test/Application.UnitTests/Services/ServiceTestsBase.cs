45c88f38a51d154b5432826712a535c3300b022e
Ylliahc <ylliahc@hotmail.fr>
2020-04-09 00:58:03 +0200

    WIP EDC test in progress

--- test/Application.UnitTests/Services/ServiceTestsBase.cs
+++ test/Application.UnitTests/Services/ServiceTestsBase.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using MaScore.EloThereUI.Domain.Entities;
 using MaScore.EloThereUI.Domain.Repositories;
 using Moq;
 
@@ -8,7 +13,7 @@ namespace MaScore.EloThereUI.Application.UnitTests.Services
     /// </summary>
     public class ServiceTestsBase
     {
-        public IScoreRepository GetScoreRepository()
+        public IScoreRepository GetScoreRepository(PlayerStatistics playerExpectedStats, Player player)
         {
             var repositoryMock = new Mock<IScoreRepository>();
 
@@ -17,9 +22,79 @@ namespace MaScore.EloThereUI.Application.UnitTests.Services
                     It.IsAny<string>(),
                     It.IsAny<string>(),
                     It.IsAny<int>()))
-                .ReturnsAsync(new System.Collections.Generic.List<Domain.Entities.Score>());
+                .ReturnsAsync(GetScores(playerExpectedStats
+                    ,player));
 
             return repositoryMock.Object;
+        }
+
+        public IPlayerRepository GetPlayerRepository(PlayerStatistics playerExpectedStats)
+        {
+            var repositoryMock = new Mock<IPlayerRepository>();
+            repositoryMock.Setup(x => x.GetPlayerAsync(It.IsAny<string>()))
+                .ReturnsAsync(GetPlayer(playerExpectedStats));
+
+            return repositoryMock.Object;
+        }
+
+        public PlayerStatistics GetExpectedPlayerStatistics()
+        {
+            var fixture = new Fixture();
+
+            var firstDate = new DateTime(2020,06,01);
+
+            return new PlayerStatistics()
+            {
+                FirstGameDate = firstDate,
+                LastGameDate = firstDate.AddMonths(6),
+                NumberGamesPlayed = 6,
+                NumberGameTypes = 3,
+                TotalPoints = 100
+            };
+        }
+
+        public List<Score> GetScores(PlayerStatistics expected,Player player)
+        {
+            var fixture = new Fixture();
+
+            var gameTypes = fixture.CreateMany<string>(expected.NumberGameTypes).ToList();
+
+            var scores = fixture.CreateMany<Score>(expected.NumberGamesPlayed).ToList();
+
+            scores[0].GameTypeId = player.GameTypes[0].Id;
+            scores[1].GameTypeId = player.GameTypes[1].Id;
+            scores[2].GameTypeId = player.GameTypes[2].Id;
+            scores[3].GameTypeId = player.GameTypes[0].Id;
+            scores[4].GameTypeId = player.GameTypes[1].Id;
+            scores[5].GameTypeId = player.GameTypes[2].Id;
+
+            scores[0].Date = expected.FirstGameDate.AddDays(1);
+            scores[1].Date = expected.FirstGameDate.AddDays(2);
+            scores[2].Date = expected.FirstGameDate.AddDays(3);
+            scores[3].Date = expected.LastGameDate;
+            scores[4].Date = expected.FirstGameDate;
+            scores[5].Date = expected.FirstGameDate.AddDays(4);
+
+            scores[0].Value = 0;
+            scores[1].Value = 25;
+            scores[2].Value = 0;
+            scores[3].Value = 50;
+            scores[4].Value = 0;
+            scores[5].Value = 25;
+
+            return scores;
+        }
+
+        public Player GetPlayer(PlayerStatistics expected)
+        {
+            var fixture = new Fixture();
+            var player = new Player()
+            {
+                LastName = "Titi",
+                FirstName = "Toto",
+                GameTypes = fixture.CreateMany<GameType>(expected.NumberGameTypes).ToList()
+            };
+            return player;
         }
     }
 }
--- test/Application.UnitTests/Services/StatisticsServiceTests.cs
+++ test/Application.UnitTests/Services/StatisticsServiceTests.cs
@@ -1,9 +1,24 @@
+using FluentAssertions;
+using MaScore.EloThereUI.Application.Services;
 using Xunit;
 
 namespace MaScore.EloThereUI.Application.UnitTests.Services
 {
     public class StatisticsServiceTests : ServiceTestsBase
     {
-
+        [Fact]
+        public void GetPlayerStatistics_When_Expect()
+        {
+            //Arrange
+            var playerExpectedStats = GetExpectedPlayerStatistics();
+            var player = GetPlayer(playerExpectedStats);
+            var scoreRepository = GetScoreRepository(playerExpectedStats, player);
+            var playerRepository = GetPlayerRepository(playerExpectedStats);
+            var statisticService = new StatisticsService(playerRepository: playerRepository,scoreRepository: scoreRepository);
+            //Act
+            var result = statisticService.GetPlayerStatistics("toto").Result;
+            //Assert
+            result.Should().BeEquivalentTo(playerExpectedStats);
+        }
     }
 }
